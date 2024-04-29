9abd4a8aaa750968face50eaaf0d0f5b2015d6db
Marc Blais <marcantblais@gmail.com>
2024-04-29 14:07:04 -0400

    corrected escapable characters warning

--- src/app/components/ScrabbleDescription.tsx
+++ src/app/components/ScrabbleDescription.tsx
@@ -8,43 +8,21 @@ import DownArrow from "./DownArrow";
 export default function ScrabbleDescription({ scrollTo }: { scrollTo: Function }) {
   
   return (
-    <div className="h-full w-full flex flex-col justify-between items-center gap-5">
-      <div className="h-full w-full pt-12 flex justify-center items-center gap-3">
-        <div className="basis-1/2 h-full flex flex-col justify-between gap-7">
-          <TextBubble className="bg-gray-200 shadow-md max-w-96">
-            <p className="text-justify">Scrabble Cheetah est une application qui vous permet de recevoir les meilleurs solutions possible pour votre grille de Scrabble.</p>            
-          </TextBubble>
+    <div className="basis-1/2 h-full flex flex-col justify-between gap-3">
+        <TextBubble className="bg-gray-200 shadow-md max-w-96">
+            Scrabble Cheetah est une application qui vous permet de recevoir les meilleurs solutions possible pour votre grille de Scrabble.            
+        </TextBubble>
 
-          <TextBubble className="bg-gray-200 shadow-md max-w-96 self-end">
-            <p className="text-justify">
-              Il est présentement possible de trouver des solutions en français et en anglais avec plusieurs type de grilles de jeu.
-            </p>       
-          </TextBubble>
+        <TextBubble className="bg-gray-200 shadow-md max-w-96 self-end">
+            Il est présentement possible de trouver des solutions en français et en anglais avec plusieurs type de grilles de jeu.
+        </TextBubble>
 
-          <TextBubble className="bg-gray-200 shadow-md max-w-96 self-center">
-            <p className="text-justify">
-              Vous pouvez sauvegarder jusqu'a 8 parties différentes sur votre compte. 
-              Il est facile d'identifier chaque partie avec un nom personnalisé et un aperçu de la grille de jeu.
-            </p>       
-          </TextBubble>
+        <TextBubble className="bg-gray-200 shadow-md max-w-96 self-center">
+            Vous pouvez sauvegarder jusqu&apos;a 8 parties différentes sur votre compte. 
+            Il est facile d&apos;identifier chaque partie avec un nom personnalisé et un aperçu de la grille de jeu.
+        </TextBubble>
 
-          <TextBubble className="bg-gray-200 shadow-md max-w-96 self-start">
-            <p className="text-justify">
-              L'interface offre une grille de jeu interactive et facile à manipuler sur PC et sur mobile.
-            </p>       
-          </TextBubble>
-
-          <small>
-            *Veuillez noter que cette démonstration n'est pas connecté au serveur et que la majorité des fonctionnalités sont innaccessibles. 
-            Pour en faire l'essaie, rendez-vous dans la section contact pour communiquer avec moi.
-          </small>
-        </div>
-        <div className="basis-1/2 h-full flex justify-center">
-          {/* <Login basis={1} /> */}
-        </div>
-      </div>
-
-      <DownArrow text={"Voir la démonstration"} action={scrollTo}/>
+        <TextBubble className="bg-gray-200 shadow-md max-w-96 self-start">L&apos;interface offre une grille de jeu interactive et facile à manipuler sur PC et sur mobile.</TextBubble>
     </div>
   )
 }
--- src/app/components/catcam/CatcamDescription.tsx
+++ src/app/components/catcam/CatcamDescription.tsx
@@ -8,10 +8,10 @@ export default function CatcamDescription() {
     return (
         <div className="p-0.5 h-full flex flex-col justify-between items-center gap-3">
             <TextBubble className="bg-gray-200">La Catcam est une application web conçue spécifiquement pour regarder vos animaux lorsque vous quittez votre foyer.</TextBubble>
-            <TextBubble className="bg-gray-200">Il est possible d'utiliser une fonctionnalité de détections de mouvements pour enregistrer les méfaits de vos félins favoris et de les prendre la main dans le sac!</TextBubble>
-            <TextBubble className="bg-gray-200">Avec la Catcam, votre vie privée est protégée puisque l'application utilise un serveur local dans votre résidence pour diffuser et enregistrer les vidéos.</TextBubble>
+            <TextBubble className="bg-gray-200">Il est possible d&apos;utiliser une fonctionnalité de détections de mouvements pour enregistrer les méfaits de vos félins favoris et de les prendre la main dans le sac!</TextBubble>
+            <TextBubble className="bg-gray-200">Avec la Catcam, votre vie privée est protégée puisque l&apos;application utilise un serveur local dans votre résidence pour diffuser et enregistrer les vidéos.</TextBubble>
             <TextBubble className="bg-gray-200">
-                L'application offre un design simple et efficace autant sur mobile que sur PC. Il est possible de personnalisé certains aspects de l'interface pour mieux répondre à vos besoins.
+                L&apos;application offre un design simple et efficace autant sur mobile que sur PC. Il est possible de personnalisé certains aspects de l&apos;interface pour mieux répondre à vos besoins.
             </TextBubble>
         </div>
     )
