11eba7777297270e647965933329f15da1414a5b
Marc Blais <marcantblais@gmail.com>
2024-05-14 13:52:34 -0400

    added meta data for sharing on social networks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,10 @@ export const metadata: Metadata = {
     title: "Marc-Antoine Blais",
     description: "Portfolio du développeur web full-stack Marc-Antoine Blais",
     other: {
-        image: "https://www.marcblais.xyz/logo.png",
         "og:image": "https://www.marcblais.xyz/logo.png",
-        url: "https://www.marcblais.xyz",
+        image: "https://www.marcblais.xyz/logo.png",
         "og:url": "https://www.marcblais.xyz",
+        url: "https://www.marcblais.xyz",
         "og:title": "Marc-Antoine Blais",
         "og:description": "Portfolio du développeur web full-stack Marc-Antoine Blais.",
         "og:type": "website",
